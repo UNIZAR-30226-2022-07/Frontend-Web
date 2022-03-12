c8940e0b42406846f95a65ddda67b8b22d63f4b1
Marcos <marcosgarralaga@gmail.com>
2022-03-12 12:46:42 +0100

    Modificado .gitignore Añadido el proyecto base, con login, register y el servicio de usuarios. Añadidos algunos TODOs pendientes de hablar con el equipo

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email: string = "";
+  password: string = "";
+  serviceError: boolean = false;
+  serviceErrorMessage: string = "";
+  userError: boolean = false;
+
+
+  constructor(public userService: UsersService, public router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    const user = {email: this.email, password: this.password};
+    this.userService.login(user).subscribe(
+      res => {
+        // TODO(Marcos): Guardar con setToken algo de res para recordar que el login es correcto. Tocar userError tambien
+        // this.userService.setToken(res.algo);
+        // NOTE(Marcos): Para borrar la cookie (hacer logout): this.cookies.delete("token");
+        this.router.navigateByUrl('/');
+      },
+      err => {
+        this.serviceError = true
+        this.serviceErrorMessage = err.message
+      }
+    );
+  }
+
+}
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  //Form
+  email: string = "";
+  password: string = "";
+  confirmPassword: string = "";
+  
+  //Error handling
+  passwordError: boolean = false;
+  serviceError: boolean = false;
+  serviceErrorMessage: string = "";
+  userError: boolean = false;
+
+  constructor(public userService: UsersService, public router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    if(this.password != this.confirmPassword) {
+      this.passwordError = true;
+      return;
+    }
+    this.passwordError = false;
+    const user = { email: this.email, password: this.password };
+    this.userService.register(user).subscribe(
+      res => {
+        // TODO(Marcos): Guardar con setToken algo de res para recordar que el login es correcto. Hacer set tambien de userError
+        // this.userService.setToken(res.algo);
+        // NOTE(Marcos): Para borrar la cookie (hacer logout): this.cookies.delete("token");
+        this.router.navigateByUrl('/');
+      },
+      err => {
+        this.serviceError = true
+        this.serviceErrorMessage = err.message
+      }
+    );
+  }
+
+}
