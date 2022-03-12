bb8190daba921a259d87945e3875a992893fe796
viictorgallardo <vicgallardo88@gmail.com>
2022-03-12 16:59:51 +0100

    Menu-v.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MenuInicialComponent } from './menu-inicial/menu-inicial.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: "", component: AppComponent, pathMatch: "full" },
   { path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "register", component: RegisterComponent, pathMatch: "full" }
+  { path: "register", component: RegisterComponent, pathMatch: "full" },
+  { path: "menuInicial", component: MenuInicialComponent, pathMatch: "full" }
+
 ];
 
 @NgModule({
