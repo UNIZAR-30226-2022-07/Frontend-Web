2c15498f84214d0d7746c41bb5f494b67f480731
Marcos <marcosgarralaga@gmail.com>
2022-03-14 19:48:08 +0100

    Correccion indentacion. Añadidos tests en login, register y userService Corregidos tests appModule

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,16 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { By } from '@angular/platform-browser';
 
 import { LoginComponent } from './login.component';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [ HttpClientTestingModule,
+                 RouterTestingModule
+               ]
     })
     .compileComponents();
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   beforeEach(() => {
@@ -21,5 +34,21 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have all form elements', () => {
+    const btn = fixture.debugElement.nativeElement.querySelector('#login-btn');
+    expect(btn.innerHTML).toBe('Iniciar sesion');
+    const input1 = fixture.debugElement.nativeElement.querySelector('#password-input');
+    expect(input1.innerHTML).toBe('');
+    const input2 = fixture.debugElement.nativeElement.querySelector('#email-input');
+    expect(input2.innerHTML).toBe('');
+  });
+
+  it('form is empty and ready at the load of the page', () => {
+    expect(component.email).toEqual("");
+    expect(component.password).toEqual("");
+    const btn = fixture.debugElement.nativeElement.querySelector('#login-btn');
+    expect(btn.disabled).toBeTruthy();
   });
 });
