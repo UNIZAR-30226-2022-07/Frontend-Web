eba766253a1112d5e82de53b99fc374e91fad37e
Marcos <marcosgarralaga@gmail.com>
2022-03-25 21:11:01 +0100

    Cambiado generacion de rotaciones aleatorias Añadido zindex a las cartas centrales

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -18,7 +18,7 @@ export class GameComponent implements OnInit {
   //Direccion del juego
   direccion:util.Direccion =  util.Direccion.NORMAL;
   //Vector de numeros aleatorios para la rotacion de las cartas de la pila central
-  randomRotation: number[] = [35, 287, 193, 9, 190, 165, 33, 141, 179, 335, 318, 173, 176, 303, 68, 292, 17, 37, 198, 43, 24, 211, 279, 102, 74, 282, 31, 101, 39, 208, 168, 13, 229, 76, 277, 52, 127, 115, 295, 178, 296, 290, 348, 192, 113, 42, 215, 324, 11, 358, 309, 83, 313, 124, 250, 289, 6, 77, 258, 46, 199, 87, 128, 53, 45, 204, 323, 314, 174, 345, 197, 103, 244, 298, 28, 16, 130, 340, 14, 272, 149, 317, 156, 104, 350, 216, 283, 136, 51, 106, 238, 66, 221, 60, 81, 79, 185, 67, 306, 359, 92, 341, 99, 123, 189, 214, 1, 134, 333, 25, 120, 180 ]
+  randomRotation: number[] = Array.from({length: 108}, () => Math.floor(Math.random() * 360)); 
   victor: Jugador = new Jugador("victor"); //Prueba
 
   constructor() { }
