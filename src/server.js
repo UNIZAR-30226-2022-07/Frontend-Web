c0f7f080180e7f2f4250606eb7979193ec9e9bd7
Marcos <marcosgarralaga@gmail.com>
2022-04-03 14:03:56 +0200

    Pruebas server.js

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/frontend-web'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/frontend-web/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
