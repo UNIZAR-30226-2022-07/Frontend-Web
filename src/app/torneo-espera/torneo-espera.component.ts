2f3a9d847d8bd6c999fc4f2b5d1e100c4c17910a
Marcos <marcosgarralaga@gmail.com>
2022-04-12 15:15:41 +0200

    Popup configuracion torneo

--- src/app/torneo-espera/torneo-espera.component.ts
+++ src/app/torneo-espera/torneo-espera.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -8,10 +9,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class TorneoEsperaComponent implements OnInit {
   tournamentID: string | null = null;
+  nJugadores: number = 2;
+  tiempoTurno: number = 10;
   players!: Array<any>;
   reglas: Array<boolean> = [false, false, false, false, false, false] //0switch, Crazy7, ProgressiveDraw, ChaosDraw, BlockDraw, RepeatDraw
 
-  constructor(private route: ActivatedRoute, public router: Router) { }
+  constructor(private route: ActivatedRoute, public router: Router, public dialog:MatDialog) { }
 
   ngOnInit(): void {
     this.tournamentID = this.route.snapshot.paramMap.get('id')
@@ -19,13 +22,13 @@ export class TorneoEsperaComponent implements OnInit {
     //Pruebas:
     this.players = [
       {
-        nombre: "3nsalada",
-        puntos: 50,
+        nombre: "Helios",
+        puntos: 73,
         pais: "España"
       },
       {
-        nombre: "Helios",
-        puntos: 73,
+        nombre: "3nsalada",
+        puntos: 50,
         pais: "España"
       },
       {
@@ -34,7 +37,6 @@ export class TorneoEsperaComponent implements OnInit {
         pais: "Ucrania"
       },
     ];
-    this.reglas = [true, true, true, true, true, true];
   }
   //Ejecutado cuando un jugador se quiere sale del torneo
   exit() {
@@ -42,4 +44,27 @@ export class TorneoEsperaComponent implements OnInit {
     this.router.navigateByUrl('/torneo')
   }
 
+  async popupAdmin() {
+    const dialogRef = this.dialog.open(AdminComponent);
+    dialogRef.componentInstance.component = this;
+  }
+
+}
+
+@Component({
+  selector: 'admin',
+  templateUrl: 'admin.html',
+  styleUrls: ['admin.css']
+})
+export class AdminComponent {
+  component!: TorneoEsperaComponent
+  constructor(public dialogRef: MatDialogRef<AdminComponent>) {}
+
+  changeNplayers(e: any) {
+    this.component.nJugadores = e.target.value;
+  }
+  
+  changeTturno(e: any) {
+    this.component.tiempoTurno = e.target.value;
+  }
 }
