f9e7e21b0346babde3980e5b1a8b4a9072d7d992
Marcos <marcosgarralaga@gmail.com>
2022-05-12 16:06:52 +0200

    Eliminado comentario inutil

--- src/app/menu-inicial/menu-inicial.component.ts
+++ src/app/menu-inicial/menu-inicial.component.ts
@@ -255,7 +255,6 @@ export class UnirsePrivada {
         this._snackBar.open("Estas ya unido a esta partida...",'');
       }
     });
-    //TODO: Checkear si te puedes unir (ya estas dentro, no hay hueco, etc...)
     await this.GameService.joinMatch(this.id).then();
     this.router.navigateByUrl('/partidaPrivada/'+this.id);
     this.dialogRef.close();
